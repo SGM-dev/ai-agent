6c3eedfd9354323ba2459c06c7f1cd93121081f4
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2026-01-03 23:38:58 +0530

    Refactor get_files_info function for improved variable naming and clarity

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,10 +5,10 @@ def get_files_info(working_directory, directory="."):
 
     try:
 
-        working_dir_abs = os.path.abspath(working_directory)
-        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        abs_working_dir = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(abs_working_dir, directory))
         valid_target_dir = (
-            os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+            os.path.commonpath([abs_working_dir, target_dir]) == abs_working_dir
         )
 
         if not valid_target_dir:
@@ -19,10 +19,10 @@ def get_files_info(working_directory, directory="."):
 
         list_of_files = []
 
-        for file in os.listdir(target_dir):
-            file_name = file
-            file_size = os.path.getsize(os.path.join(target_dir, file))
-            is_dir = os.path.isdir(os.path.join(target_dir, file))
+        for file_name in os.listdir(target_dir):
+            file_path = os.path.join(target_dir, file_name)
+            file_size = os.path.getsize(file_path)
+            is_dir = os.path.isdir(file_path)
             list_of_files.append(
                 f"- {file_name}: file_size={file_size}, is_dir={is_dir}"
             )
